89ca9ffea204611e4254ac8768722d27c83cd3e6
Simon Cropp <simon.cropp@gmail.com>
2019-06-24 13:23:58 +1000

    add scrubber

--- src/NServiceBus.ApprovalTests/NsbContextVerifier.cs
+++ src/NServiceBus.ApprovalTests/NsbContextVerifier.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using NServiceBus.Extensibility;
 using NServiceBus.ObjectBuilder;
 using NServiceBus.Testing;
@@ -39,17 +40,17 @@ namespace NServiceBus.ApprovalTests
             return settings;
         }
 
-        public static void Verify(this TestableAuditContext context, object state = null)
+        public static void Verify(this TestableAuditContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        static void InnerVerify(object context, object state)
+        static void InnerVerify(object context, object state, Func<string, string> scrubber)
         {
             Guard.AgainstNull(context, nameof(context));
             if (state == null)
             {
-                ObjectApprover.VerifyWithJson(context, jsonSerializerSettings: BuildSerializer());
+                ObjectApprover.VerifyWithJson(context, jsonSerializerSettings: BuildSerializer(), scrubber: scrubber);
                 return;
             }
 
@@ -58,122 +59,122 @@ namespace NServiceBus.ApprovalTests
                 NsbTestContext = context,
                 ExtraState = state
             };
-            ObjectApprover.VerifyWithJson(wrapper, jsonSerializerSettings: BuildSerializer());
+            ObjectApprover.VerifyWithJson(wrapper, jsonSerializerSettings: BuildSerializer(), scrubber: scrubber);
         }
 
-        public static void Verify(this TestableBatchDispatchContext context, object state = null)
+        public static void Verify(this TestableBatchDispatchContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableBehaviorContext context, object state = null)
+        public static void Verify(this TestableBehaviorContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableDispatchContext context, object state = null)
+        public static void Verify(this TestableDispatchContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableEndpointInstance context, object state = null)
+        public static void Verify(this TestableEndpointInstance context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableForwardingContext context, object state = null)
+        public static void Verify(this TestableForwardingContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableIncomingLogicalMessageContext context, object state = null)
+        public static void Verify(this TestableIncomingLogicalMessageContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableIncomingPhysicalMessageContext context, object state = null)
+        public static void Verify(this TestableIncomingPhysicalMessageContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableInvokeHandlerContext context, object state = null)
+        public static void Verify(this TestableInvokeHandlerContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableMessageHandlerContext context, object state = null)
+        public static void Verify(this TestableMessageHandlerContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableMessageProcessingContext context, object state = null)
+        public static void Verify(this TestableMessageProcessingContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableMessageSession context, object state = null)
+        public static void Verify(this TestableMessageSession context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingContext context, object state = null)
+        public static void Verify(this TestableOutgoingContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingLogicalMessageContext context, object state = null)
+        public static void Verify(this TestableOutgoingLogicalMessageContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingPhysicalMessageContext context, object state = null)
+        public static void Verify(this TestableOutgoingPhysicalMessageContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingPublishContext context, object state = null)
+        public static void Verify(this TestableOutgoingPublishContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingReplyContext context, object state = null)
+        public static void Verify(this TestableOutgoingReplyContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableOutgoingSendContext context, object state = null)
+        public static void Verify(this TestableOutgoingSendContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestablePipelineContext context, object state = null)
+        public static void Verify(this TestablePipelineContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableRoutingContext context, object state = null)
+        public static void Verify(this TestableRoutingContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableSubscribeContext context, object state = null)
+        public static void Verify(this TestableSubscribeContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableTransportReceiveContext context, object state = null)
+        public static void Verify(this TestableTransportReceiveContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestableUnsubscribeContext context, object state = null)
+        public static void Verify(this TestableUnsubscribeContext context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
 
-        public static void Verify(this TestingLoggerFactory context, object state = null)
+        public static void Verify(this TestingLoggerFactory context, object state = null, Func<string, string> scrubber = null)
         {
-            InnerVerify(context, state);
+            InnerVerify(context, state, scrubber);
         }
     }
 }
