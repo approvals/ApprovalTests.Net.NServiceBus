06d36baba81c3498afba80c36d4b0878c37fb9f4
Simon Cropp <simon.cropp@gmail.com>
2019-10-16 20:08:51 +1100

    doco

---
+++ src/Tests/Snippets/MessageHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.ApprovalTests;
+using NServiceBus.Testing;
+using Xunit;
+
+public class MessageHandlerTests
+{
+    #region HandlerTest
+    [Fact]
+    public async Task VerifyHandlerResult()
+    {
+        var handler = new MyHandler();
+        var context = new TestableMessageHandlerContext();
+
+        await handler.Handle(new MyRequest(), context)
+            .ConfigureAwait(false);
+
+        context.Verify();
+    }
+    #endregion
+}
